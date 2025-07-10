437fd315176aaa8b50a35f7a6650d3ab151d0b6c
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-07-11 02:06:20 +0600

    update

--- scirpt.js
+++ scirpt.js
@@ -1,11 +1,22 @@
-const menu= document.querySelector('.menu-btn')
-const head= document.querySelector('.header-nav')
-const close= document.querySelector('.close-icon')
+const menu = document.querySelector(".menu-btn");
+const head = document.querySelector(".header-nav");
+const close = document.querySelector(".close-icon");
 
-menu.addEventListener('click', function(){
-  head.style.top= '0%'
-})
+menu.addEventListener("click", function () {
+  head.style.top = "0%";
+});
 
-close.addEventListener('click', function(){
-  head.style.top= '-100%'
-})
+close.addEventListener("click", function () {
+  head.style.top = "-100%";
+});
+
+const searchBtn = document.querySelector(".search-btn");
+const searchModal = document.getElementById("search-modal");
+
+searchBtn.addEventListener("click", function () {
+  searchModal.style.display = "flex";
+});
+
+function closeSearch() {
+  searchModal.style.display = "none";
+}
